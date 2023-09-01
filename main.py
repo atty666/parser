50ab765f9dd0b99d9ce76c5fc83fc9c1c2f561b0
denys <supriadkin.d@gmail.com>
2023-09-01 13:01:00 +0300

    Init

---
+++ main.py
@@ -0,0 +1,13 @@
+from fake_useragent import UserAgent
+import requests
+
+user_agent = UserAgent()
+
+def collect_data():
+    pass
+
+def main():
+    collect_data()
+
+if __name__ == '__main__':
+    main()
