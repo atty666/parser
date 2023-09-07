6a66f480898ebf4b2ecbd191e567be3f8993d665
denys <supriadkin.d@gmail.com>
2023-09-07 19:28:25 +0300

    release

--- bot.py
+++ bot.py
@@ -1,24 +1,27 @@
+import time
 from aiogram import Bot, Dispatcher, F
 from  aiogram.types import Message
+from aiogram.utils.markdown import hbold, hlink
 import asyncio
 import logging
 from  tokens import TOKEN
 
+
 def post_to_telegram(products):
     logging.basicConfig(level=logging.INFO)
 
-    bot = Bot(token=TOKEN)
+    bot = Bot(token=TOKEN, parse_mode='HTML')
     dp = Dispatcher()
 
     @dp.message(F.text == '/start')
     async def cmd_start(message:Message):
         await message.answer(f'Hello, {message.from_user.first_name} !')
         for product in products:
-            await message.answer(product['title'])#, \
-                                 #product['price'], \
-                                 #product['status'], \
-                                 #product['link'])
-
+            card = f' {hlink(product["title"], product["link"])}\n' \
+                f'{hbold("Price:")} {product["price"]}\n' \
+                f'{hbold("Status:")} {product["status"]}'
+            await message.answer(card)
+            time.sleep(1)
 
     async def starting():
         await dp.start_polling(bot)
